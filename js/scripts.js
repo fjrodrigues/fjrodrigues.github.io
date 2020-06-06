81338848123ca82bbd21a5b11e62807e95347d81
Filipe Rodrigues <filipe.rodrigues@strypes.pt>
2020-06-06 20:50:44 +0100

    unminified js and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,100 @@
-var a=['height','getDocument','pdfjs-dist/build/pdf','workerSrc','GlobalWorkerOptions','getContext','numPages','page','getViewport','https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.4.456/pdf.worker.js','getElementById','dataset','then','width','target','https://raw.githubusercontent.com/fjrodrigues/fjrodrigues.github.io/master/pdf/AnachronicStamp.pdf','addEventListener','the-canvas','prev','getPage','promise'];(function(b,e){var f=function(g){while(--g){b['push'](b['shift']());}};f(++e);}(a,0xa1));var b=function(c,d){c=c-0x0;var e=a[c];return e;};var url=b('0x1');var pdfjsLib=window[b('0x9')];pdfjsLib[b('0xb')][b('0xa')]=b('0x10');var pdfDoc=null,pageNum=0x1,pageRendering=![],pageNumPending=null,scale=0x1,canvas=document[b('0x11')](b('0x3')),ctx=canvas[b('0xc')]('2d');function renderPage(c){pageRendering=!![];pdfDoc[b('0x5')](c)[b('0x13')](function(d){var e=d[b('0xf')]({'scale':scale});canvas[b('0x7')]=e[b('0x7')];canvas[b('0x14')]=e[b('0x14')];var f={'canvasContext':ctx,'viewport':e};var g=d['render'](f);g[b('0x6')][b('0x13')](function(){pageRendering=![];if(pageNumPending!==null){renderPage(pageNumPending);pageNumPending=null;}});});}function queueRenderPage(c){if(pageRendering){pageNumPending=c;}else{renderPage(c);}}function onPrevPage(){if(pageNum<=0x1){return;}pageNum--;queueRenderPage(pageNum);}document[b('0x11')](b('0x4'))[b('0x2')]('click',onPrevPage);function onNextPage(){if(pageNum>=pdfDoc[b('0xd')]){return;}pageNum++;queueRenderPage(pageNum);}document[b('0x11')]('next')[b('0x2')]('click',onNextPage);function goToPage(){pageNum=parseInt(event[b('0x0')][b('0x12')][b('0xe')]);renderPage(pageNum);}pdfjsLib[b('0x8')](url)[b('0x6')][b('0x13')](function(c){pdfDoc=c;renderPage(pageNum);});
+// If absolute URL from the remote server is provided, configure the CORS
+// header on that server.
+var url = 'https://raw.githubusercontent.com/fjrodrigues/fjrodrigues.github.io/master/pdf/AnachronicStamp.pdf';
+//var url = 'https://github.com/fjrodrigues/fjrodrigues.github.io/raw/master/pdf/AnachronicStamp.pdf';
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.4.456/pdf.worker.js';
+
+var pdfDoc = null,
+    pageNum = 1,
+    pageRendering = false,
+    pageNumPending = null,
+    scale = 1.0,
+    canvas = document.getElementById('the-canvas'),
+    ctx = canvas.getContext('2d');
+
+/**
+ * Get page info from document, resize canvas accordingly, and render page.
+ * @param num Page number.
+ */
+function renderPage(num) {
+  pageRendering = true;
+  // Using promise to fetch the page
+  pdfDoc.getPage(num).then(function(page) {
+    var viewport = page.getViewport({scale: scale});
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Render PDF page into canvas context
+    var renderContext = {
+      canvasContext: ctx,
+      viewport: viewport
+    };
+    var renderTask = page.render(renderContext);
+
+    // Wait for rendering to finish
+    renderTask.promise.then(function() {
+      pageRendering = false;
+      if (pageNumPending !== null) {
+        // New page rendering is pending
+        renderPage(pageNumPending);
+        pageNumPending = null;
+      }
+    });
+  });
+
+  // Update page counters
+  //document.getElementById('page_num').textContent = num;
+}
+
+/**
+ * If another page rendering in progress, waits until the rendering is
+ * finised. Otherwise, executes rendering immediately.
+ */
+function queueRenderPage(num) {
+  if (pageRendering) {
+    pageNumPending = num;
+  } else {
+    renderPage(num);
+  }
+}
+
+/**
+ * Displays previous page.
+ */
+function onPrevPage() {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+}
+document.getElementById('prev').addEventListener('click', onPrevPage);
+
+/**
+ * Displays next page.
+ */
+function onNextPage() {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+}
+document.getElementById('next').addEventListener('click', onNextPage);
+
+function goToPage() {
+	pageNum = parseInt(event.target.dataset.page);
+	renderPage(pageNum);
+}
+/**
+ * Asynchronously downloads PDF.
+ */
+pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+  pdfDoc = pdfDoc_;
+  renderPage(pageNum);
+});
