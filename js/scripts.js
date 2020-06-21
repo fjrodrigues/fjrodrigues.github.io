c694220c645c65e7ab5a1318d81f6b8206cd6a1e
Filipe Rodrigues <filipe.rodrigues@strypes.pt>
2020-06-21 14:57:35 +0100

    changed manual gallery, consolidated js in a single file

--- js/scripts.js
+++ js/scripts.js
@@ -1,100 +1,133 @@
-// If absolute URL from the remote server is provided, configure the CORS
-// header on that server.
-var url = 'https://raw.githubusercontent.com/fjrodrigues/fjrodrigues.github.io/master/pdf/AnachronicStamp.pdf';
-//var url = 'https://github.com/fjrodrigues/fjrodrigues.github.io/raw/master/pdf/AnachronicStamp.pdf';
 
-// Loaded via <script> tag, create shortcut to access PDF.js exports.
-var pdfjsLib = window['pdfjs-dist/build/pdf'];
+const baseImageSource = 'images/manual/AnachronicStamp_page-';
+const fileExtension = '.jpg';
+var currentPage = 1;
 
-// The workerSrc property shall be specified.
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.4.456/pdf.worker.js';
-
-var pdfDoc = null,
-    pageNum = 1,
-    pageRendering = false,
-    pageNumPending = null,
-    scale = 1.0,
-    canvas = document.getElementById('the-canvas'),
-    ctx = canvas.getContext('2d');
-
-/**
- * Get page info from document, resize canvas accordingly, and render page.
- * @param num Page number.
- */
-function renderPage(num) {
-  pageRendering = true;
-  // Using promise to fetch the page
-  pdfDoc.getPage(num).then(function(page) {
-    var viewport = page.getViewport({scale: scale});
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    // Render PDF page into canvas context
-    var renderContext = {
-      canvasContext: ctx,
-      viewport: viewport
-    };
-    var renderTask = page.render(renderContext);
-
-    // Wait for rendering to finish
-    renderTask.promise.then(function() {
-      pageRendering = false;
-      if (pageNumPending !== null) {
-        // New page rendering is pending
-        renderPage(pageNumPending);
-        pageNumPending = null;
-      }
-    });
-  });
-
-  // Update page counters
-  //document.getElementById('page_num').textContent = num;
+function nextImage() {
+	this.currentPage++;
+	const formatedNumber = formatNumber(this.currentPage);
+	setPageNumber(formatedNumber);
 }
 
-/**
- * If another page rendering in progress, waits until the rendering is
- * finised. Otherwise, executes rendering immediately.
- */
-function queueRenderPage(num) {
-  if (pageRendering) {
-    pageNumPending = num;
-  } else {
-    renderPage(num);
-  }
+function previousImage(){
+	if(this.currentPage != 1) {
+		this.currentPage--;
+		const formatedNumber = formatNumber(this.currentPage);
+		setPageNumber(formatedNumber);
+	}
 }
 
-/**
- * Displays previous page.
- */
-function onPrevPage() {
-  if (pageNum <= 1) {
-    return;
-  }
-  pageNum--;
-  queueRenderPage(pageNum);
+function setPageNumber(pageNumber){
+	const newImageSource = baseImageSource + pageNumber + fileExtension;
+	document.getElementById('manual').src = newImageSource;
 }
-document.getElementById('prev').addEventListener('click', onPrevPage);
 
-/**
- * Displays next page.
- */
-function onNextPage() {
-  if (pageNum >= pdfDoc.numPages) {
-    return;
-  }
-  pageNum++;
-  queueRenderPage(pageNum);
+function formatNumber(numberToFormat){
+	if(numberToFormat <10){
+		return '000' + numberToFormat;
+	}
+	if(numberToFormat <100){
+		return '00' + numberToFormat;
+	}
+	if(numberToFormat <1000){
+		return '0' + numberToFormat;
+	}
+	return numberToFormat;
 }
-document.getElementById('next').addEventListener('click', onNextPage);
 
 function goToPage() {
-	pageNum = parseInt(event.target.dataset.page);
-	renderPage(pageNum);
+	this.currentPage = parseInt(event.target.dataset.page);
+	const formatedNumber = formatNumber(this.currentPage);
+	setPageNumber(formatedNumber);
 }
-/**
- * Asynchronously downloads PDF.
- */
-pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
-  pdfDoc = pdfDoc_;
-  renderPage(pageNum);
-});
+
+var activeItem;
+function toggleActive() {
+	const eventSource = event.target.dataset.target;
+	let newActiveItem = "phase-" + eventSource;
+	if(!this.activeItem || this.activeItem == newActiveItem)
+	{
+		var elements = Array.from(document.getElementsByClassName(newActiveItem));
+		elements.forEach(element => element.classList.toggle("active-item"));
+	}
+	else
+	{
+		var elements = Array.from(document.getElementsByClassName(this.activeItem));
+		elements.forEach(element => element.classList.remove("active-item"));
+		var newElements = Array.from(document.getElementsByClassName(newActiveItem));
+		newElements.forEach(element => element.classList.toggle("active-item"));
+	}
+	this.activeItem = newActiveItem;
+	toggleFictionBlock(eventSource);
+}
+function toggleFictionBlock(target) {
+	const targetClassIdentifier = target
+	let timelineBlockElements = document.getElementsByClassName('timeline-block');
+	for(let j=0; j<timelineBlockElements.length; j++) {
+		if(timelineBlockElements[j].classList.contains(targetClassIdentifier))
+		{
+			timelineBlockElements[j].classList.toggle('d-none');
+			scrollToSection();
+			if(timelineBlockElements[j].classList.contains('d-none')) {
+				pauseSelfVideo(targetClassIdentifier);
+			}
+		}
+		else if(!timelineBlockElements[j].classList.contains('d-none')) {
+			timelineBlockElements[j].classList.add('d-none');
+			pauseOthers(targetClassIdentifier);
+		}
+	}
+}
+
+function scrollToSection(){
+	$('html, body').stop(true, false).animate({ scrollTop: $('#video_section').offset().top}, 800);
+}
+
+function pauseSelfVideo( target ) {
+	videoPlayerId = 'video_' + target;
+	document.getElementById(videoPlayerId).pause();
+}
+
+function pauseOthers( excludePlayer) {
+	let listOfPlayersIds = ["video_pair-1", "video_pair-2", "video_pair-3", "video_pair-4"];
+	for(element in listOfPlayersIds){
+		if (listOfPlayersIds[element] != excludePlayer){
+			document.getElementById(listOfPlayersIds[element]).pause();
+		}
+	}
+}
+
+var activeItemTimeline;
+function toggleVisibility() {
+	let target = event.target;
+	if(!this.activeItemTimeline || this.activeItemTimeline == target.id)
+	{
+		target.classList.toggle("active-timeline");
+	}
+	else
+	{
+		var element = document.getElementById(this.activeItemTimeline);
+		element.classList.remove("active-timeline");
+		target.classList.toggle("active-timeline");
+	}
+	this.activeItemTimeline = target.id;
+	toggleTimeBlock(target.dataset.target);
+}
+function toggleTimeBlock( target ) {
+	const targetClassIdentifier = target;
+	let timelineBlockElements = document.getElementsByClassName('timeline-block');
+	for(let j=0; j<timelineBlockElements.length; j++) {
+		if(timelineBlockElements[j].classList.contains(targetClassIdentifier))
+		{
+			timelineBlockElements[j].classList.toggle('d-none');
+			scrollToSection();
+			if(timelineBlockElements[j].classList.contains('d-none')) {
+				pauseSelfVideo(targetClassIdentifier);
+			}
+		}
+		else if(!timelineBlockElements[j].classList.contains('d-none')) {
+			timelineBlockElements[j].classList.add('d-none');
+			pauseOthers(targetClassIdentifier);
+		}
+	}
+}
