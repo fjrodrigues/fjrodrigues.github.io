bb8ea3b39a270d09e34173bea1a1b3c7c72e8ce5
Filipe Rodrigues <filipe.rodrigues@strypes.pt>
2020-07-07 20:34:09 +0100

    changes after Av

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,38 @@
-
-const baseImageSource = 'images/manual/AnachronicStamp_page-';
-const fileExtension = '.jpg';
-const maxPageNumber = 57;
+const baseImageSource = "images/manual/AnachronicStamp_page-";
+const fileExtension = ".jpg";
+const maxPageNumber = 63;
 var currentPage = 1;
 
 function nextImage() {
-	if(this.currentPage < maxPageNumber){
+	if (this.currentPage < maxPageNumber) {
 		this.currentPage++;
 		const formatedNumber = formatNumber(this.currentPage);
 		setPageNumber(formatedNumber);
 	}
 }
 
-function previousImage(){
-	if(this.currentPage != 1) {
+function previousImage() {
+	if (this.currentPage != 1) {
 		this.currentPage--;
 		const formatedNumber = formatNumber(this.currentPage);
 		setPageNumber(formatedNumber);
 	}
 }
 
-function setPageNumber(pageNumber){
+function setPageNumber(pageNumber) {
 	const newImageSource = baseImageSource + pageNumber + fileExtension;
-	document.getElementById('manual').src = newImageSource;
+	document.getElementById("manual").src = newImageSource;
 }
 
-function formatNumber(numberToFormat){
-	if(numberToFormat <10){
-		return '000' + numberToFormat;
+function formatNumber(numberToFormat) {
+	if (numberToFormat < 10) {
+		return "000" + numberToFormat;
 	}
-	if(numberToFormat <100){
-		return '00' + numberToFormat;
+	if (numberToFormat < 100) {
+		return "00" + numberToFormat;
 	}
-	if(numberToFormat <1000){
-		return '0' + numberToFormat;
+	if (numberToFormat < 1000) {
+		return "0" + numberToFormat;
 	}
 	return numberToFormat;
 }
@@ -48,53 +47,67 @@ var activeItem;
 function toggleActive() {
 	const eventSource = event.target.dataset.target;
 	let newActiveItem = "phase-" + eventSource;
-	if(!this.activeItem || this.activeItem == newActiveItem)
-	{
-		var elements = Array.from(document.getElementsByClassName(newActiveItem));
-		elements.forEach(element => element.classList.toggle("active-item"));
-	}
-	else
-	{
-		var elements = Array.from(document.getElementsByClassName(this.activeItem));
-		elements.forEach(element => element.classList.remove("active-item"));
-		var newElements = Array.from(document.getElementsByClassName(newActiveItem));
-		newElements.forEach(element => element.classList.toggle("active-item"));
+	if (!this.activeItem || this.activeItem == newActiveItem) {
+		var elements = Array.from(
+			document.getElementsByClassName(newActiveItem)
+		);
+		elements.forEach((element) => element.classList.toggle("active-item"));
+	} else {
+		var elements = Array.from(
+			document.getElementsByClassName(this.activeItem)
+		);
+		elements.forEach((element) => element.classList.remove("active-item"));
+		var newElements = Array.from(
+			document.getElementsByClassName(newActiveItem)
+		);
+		newElements.forEach((element) =>
+			element.classList.toggle("active-item")
+		);
 	}
 	this.activeItem = newActiveItem;
 	toggleFictionBlock(eventSource);
 }
 function toggleFictionBlock(target) {
-	const targetClassIdentifier = target
-	let timelineBlockElements = document.getElementsByClassName('timeline-block');
-	for(let j=0; j<timelineBlockElements.length; j++) {
-		if(timelineBlockElements[j].classList.contains(targetClassIdentifier))
-		{
-			timelineBlockElements[j].classList.toggle('d-none');
+	const targetClassIdentifier = target;
+	let timelineBlockElements = document.getElementsByClassName(
+		"timeline-block"
+	);
+	for (let j = 0; j < timelineBlockElements.length; j++) {
+		if (
+			timelineBlockElements[j].classList.contains(targetClassIdentifier)
+		) {
+			timelineBlockElements[j].classList.toggle("d-none");
 			scrollToSection();
-			if(timelineBlockElements[j].classList.contains('d-none')) {
+			if (timelineBlockElements[j].classList.contains("d-none")) {
 				pauseSelfVideo(targetClassIdentifier);
 			}
-		}
-		else if(!timelineBlockElements[j].classList.contains('d-none')) {
-			timelineBlockElements[j].classList.add('d-none');
+		} else if (!timelineBlockElements[j].classList.contains("d-none")) {
+			timelineBlockElements[j].classList.add("d-none");
 			pauseOthers(targetClassIdentifier);
 		}
 	}
 }
 
-function scrollToSection(){
-	$('html, body').stop(true, false).animate({ scrollTop: $('#video_section').offset().top}, 800);
+function scrollToSection() {
+	$("html, body")
+		.stop(true, false)
+		.animate({ scrollTop: $("#video_section").offset().top }, 800);
 }
 
-function pauseSelfVideo( target ) {
-	videoPlayerId = 'video_' + target;
+function pauseSelfVideo(target) {
+	videoPlayerId = "video_" + target;
 	document.getElementById(videoPlayerId).pause();
 }
 
-function pauseOthers( excludePlayer) {
-	let listOfPlayersIds = ["video_pair-1", "video_pair-2", "video_pair-3", "video_pair-4"];
-	for(element in listOfPlayersIds){
-		if (listOfPlayersIds[element] != excludePlayer){
+function pauseOthers(excludePlayer) {
+	let listOfPlayersIds = [
+		"video_pair-1",
+		"video_pair-2",
+		"video_pair-3",
+		"video_pair-4"
+	];
+	for (element in listOfPlayersIds) {
+		if (listOfPlayersIds[element] != excludePlayer) {
 			document.getElementById(listOfPlayersIds[element]).pause();
 		}
 	}
@@ -103,12 +116,9 @@ function pauseOthers( excludePlayer) {
 var activeItemTimeline;
 function toggleVisibility() {
 	let target = event.target;
-	if(!this.activeItemTimeline || this.activeItemTimeline == target.id)
-	{
+	if (!this.activeItemTimeline || this.activeItemTimeline == target.id) {
 		target.classList.toggle("active-timeline");
-	}
-	else
-	{
+	} else {
 		var element = document.getElementById(this.activeItemTimeline);
 		element.classList.remove("active-timeline");
 		target.classList.toggle("active-timeline");
@@ -116,20 +126,22 @@ function toggleVisibility() {
 	this.activeItemTimeline = target.id;
 	toggleTimeBlock(target.dataset.target);
 }
-function toggleTimeBlock( target ) {
+function toggleTimeBlock(target) {
 	const targetClassIdentifier = target;
-	let timelineBlockElements = document.getElementsByClassName('timeline-block');
-	for(let j=0; j<timelineBlockElements.length; j++) {
-		if(timelineBlockElements[j].classList.contains(targetClassIdentifier))
-		{
-			timelineBlockElements[j].classList.toggle('d-none');
+	let timelineBlockElements = document.getElementsByClassName(
+		"timeline-block"
+	);
+	for (let j = 0; j < timelineBlockElements.length; j++) {
+		if (
+			timelineBlockElements[j].classList.contains(targetClassIdentifier)
+		) {
+			timelineBlockElements[j].classList.toggle("d-none");
 			scrollToSection();
-			if(timelineBlockElements[j].classList.contains('d-none')) {
+			if (timelineBlockElements[j].classList.contains("d-none")) {
 				pauseSelfVideo(targetClassIdentifier);
 			}
-		}
-		else if(!timelineBlockElements[j].classList.contains('d-none')) {
-			timelineBlockElements[j].classList.add('d-none');
+		} else if (!timelineBlockElements[j].classList.contains("d-none")) {
+			timelineBlockElements[j].classList.add("d-none");
 			pauseOthers(targetClassIdentifier);
 		}
 	}
